495e4ab58311a5c8d8f0309bf3e1ebbcfbfac1cf
Blaine Heffron <baheffron@gmail.com>
2025-06-25 15:12:21 -0400

    add some additional args for testing

--- contracts/fungible/src/contract.rs
+++ contracts/fungible/src/contract.rs
@@ -2,21 +2,35 @@
 // Compatible with OpenZeppelin Stellar Soroban Contracts ^0.2.0
 
 
-use soroban_sdk::{Address, contract, contractimpl, Env, String, Symbol, symbol_short};
+use soroban_sdk::{Address, contract, contractimpl, contracttype, Env, String, Symbol, symbol_short};
 use stellar_fungible::{
     self as fungible, burnable::FungibleBurnable, FungibleToken, mintable::FungibleMintable
 };
 
 const OWNER: Symbol = symbol_short!("OWNER");
+const MYBOOL: Symbol = symbol_short!("MYBOOL");
+const MYENUM: Symbol = symbol_short!("MYENUM");
+
+#[contracttype]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(i8)]
+pub enum TestMode {
+    Development = 0,
+    Testing = 1,
+    Production = 2,
+}
 
 #[contract]
 pub struct MyToken;
 
 #[contractimpl]
 impl MyToken {
-    pub fn __constructor(e: &Env, owner: Address) {
+    pub fn __constructor(e: &Env, owner: Address, initial_supply: i128, mybool: bool, myenum: TestMode) {
         fungible::metadata::set_metadata(e, 18, String::from_str(e, "MyToken"), String::from_str(e, "MTK"));
+        fungible::mintable::mint(e, &owner, initial_supply);
         e.storage().instance().set(&OWNER, &owner);
+        e.storage().instance().set(&MYBOOL, &mybool);
+        e.storage().instance().set(&MYENUM, &myenum);
     }
 }
 
